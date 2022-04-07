52214d05e84dc07d5a8a99157b58539f3bf8e10f
RyanMacc <arbader2142@gmail.com>
2022-04-07 12:32:59 -0400

    Basic HTML CSS JS good, collected everything in jQuery variables

--- scripts/app.js
+++ scripts/app.js
@@ -1,8 +1,36 @@
 // js link check
 // console.log('hello front end')
 
-// collecting cells in a variable
+//Global Variables
 
-let $cells = $('.cell')
-console.log($cells)
-console.log($cells.length)
+// collecting all cells in a variable
+let $cells = $('.cell');
+// console.log($cells)
+// console.log($cells.length)
+
+// collecting hidden cells (starting) in variable
+let $hiddenCells = $('.hidden');
+// console.log($hiddenCells)
+// console.log($hiddenCells.length)
+
+// collecting revealed cells (with black text) in variable
+let $revealedCells = $('.revealed');
+// console.log($revealedCells)
+// console.log($revealedCells.length)
+
+// collecting bomb cells in a variable
+let $bombCells = $('.bomb');
+// console.log($bombCells)
+// console.log($bombCells.length)
+
+//collecting diffused cells in a variable
+let $diffusedCells = $('.diffused');
+// console.log($diffusedCells);
+// console.log($diffusedCells.length);
+
+
+
+
+
+
+
