709f089763f578a2dc11e92f0ffafbaf80713574
RyanMacc <arbader2142@gmail.com>
2022-04-07 12:08:45 -0400

    created cell class to manipulate game data and collected into jQuery variable

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,8 @@
-console.log('hello front end')
+// js link check
+// console.log('hello front end')
 
+// collecting cells in a variable
+
+let $cells = $('.cell')
+console.log($cells)
+console.log($cells.length)
