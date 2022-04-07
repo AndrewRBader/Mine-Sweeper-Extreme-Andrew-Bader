b1ea83a27db67ff47f30b16eb92ec81f05ed30de
RyanMacc <arbader2142@gmail.com>
2022-04-07 11:26:55 -0400

    4x4 gameboard with header and some flex style

--- scripts/app.js
+++ scripts/app.js
@@ -1,17 +1,2 @@
 console.log('hello front end')
 
-let $h1El = $('h1')
-console.log($h1El)
-$h1El.hide();
-
-const $activate = $('.activated')
-const $cleared = $('.cleared')
-
-$activate.click(() => {
-    $h1El.fadeIn();
-    console.log('jQuery activated')
-})
-
-$cleared.click(() => {
-    $h1El.fadeOut();
-})
