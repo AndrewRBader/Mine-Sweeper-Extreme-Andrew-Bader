e6e3f258904d4fb584633433646753ef245f1bc0
RyanMacc <arbader2142@gmail.com>
2022-04-07 20:22:57 -0400

    added variables that contain coordinates of clicked cell (empty 0 cell)

--- scripts/app.js
+++ scripts/app.js
@@ -56,29 +56,38 @@ cells.forEach(cell => {
         if (cell.innerHTML === 'Bomb') {
             cell.classList.add('revealed')
 
-            // Console logs to play in console, alert to play in browser link
+            // Console logs to play in console
             console.log('You Lose! Game Over!')
-            alert('You Lose! Game Over!')
             console.log(cell)
         }
         else if (parseInt(cell.innerHTML) > 0 && parseInt(cell.innerHTML) <= 2) { 
             cell.classList.add('revealed')
 
-            // Console logs to play in console, alert to play in browser link
+            // Console logs to play in console
             console.log(`${cell.innerHTML} bombs are close`)
-            alert(`${cell.innerHTML} bombs are close`)
             console.log(cell)
         }
         else if (parseInt(cell.innerHTML) === 0){
             cell.classList.add('revealed')
 
-            // Console logs to play in console, alert to play in browser link
+            // Console logs to play in console
             console.log('no bombs are near')
-            alert('no bombs are near')
             console.log(cell)
+
+            //find coordinates of click
+            const col = parseInt(cell.getAttribute('id').split('-').slice(3))
+            console.log(col)
+            const row = parseInt(cell.getAttribute('id').split('-').slice(1,2))
+            console.log(row)
+            const clickCoordinates = [col, row];
+            console.log(clickCoordinates)
+
         }
     })
 })
+
+
+
 
 
 
