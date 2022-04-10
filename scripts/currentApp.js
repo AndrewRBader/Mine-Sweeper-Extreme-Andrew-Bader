1d956f181741f4f41204a0e1264bf87edf225396
RyanMacc <arbader2142@gmail.com>
2022-04-10 15:50:42 -0400

    up through shuffling the game cell tiles (bombs and empty cells)

--- scripts/currentApp.js
+++ scripts/currentApp.js
@@ -7,24 +7,14 @@ const gameBoard = document.querySelector('.cellfield')
 let width = 10 
 let numberOfCells = width*width;
 
-// # of bombs to input into game board
 
-const numberOfBombs = 20;
-const bombsArray = [];
 
-// giving bomb array 'bomb' strings
 
-for (let bomb = 0; bomb<numberOfBombs; bomb++){
-    bombsArray.push('bomb')
-}
-
-console.log(bombsArray)
 
 //used to collect custom gameboard tiles
 let cellEls = [];
 
 //custom board creation:
-
 function customBoardGeneration () {
     for (let cell = 0; cell < numberOfCells; cell++){
         const cellEl = document.createElement('div');
@@ -40,12 +30,44 @@ function customBoardGeneration () {
 customBoardGeneration()
 
 
+//creating game board tiles of bombs and empty cells
+
+
+function gameTilesGeneration () {
+
+// bomb tile generation
+// # of bombs to input into game board
+
+const numberOfBombs = 20;
+const bombsArray = [];
+
+// giving bomb array 'bomb' strings
+
+for (let bomb = 0; bomb<numberOfBombs; bomb++){
+    bombsArray.push('bomb')
+}
+
+//Empty tile generation
+
 //used to collect empty cells that are not bombs
+const emptyCellArray = [];
+const emptyCellElsNumber = numberOfCells - numberOfBombs;
 
-let emptyCellEls = [numberOfCells = numberOfBombs];
+for (let emptyCellEl = 0; emptyCellEl < emptyCellElsNumber; emptyCellEl++){
+    emptyCellArray.push('empty');
+}
 
+//combining the empty and bomb cells to create a "filled array" of bombs and empty squares
+const customGameBoardArray = emptyCellArray.concat(bombsArray)
+//randomizing the tiles of the game board array
+const randomGameBoardArray = customGameBoardArray.sort(() => Math.random() -0.5)
+console.log(randomGameBoardArray)
 
+}
 
+gameTilesGeneration()
+
+// randomizing the game tiles for random distribution within custom gameboard
 
 
 })
